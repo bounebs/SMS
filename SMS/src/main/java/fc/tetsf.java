e4f0cdf5ad15f3d1c6c88e292390f4b84e850e44
bounebs <73826993+bounebs@users.noreply.github.com>
2024-01-22 16:07:26 +0100

    commit 1

---
+++ SMS/src/main/java/fc/tetsf.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fc;
+
+/**
+ *
+ * @author hrbou
+ */
+public class tetsf {
+    private static int i = 0;
+    private int j = 3;
+    private int h ;
+    
+    public tetsf(int i, int j){
+        h = i + j;
+    }
+    
+    public void methode1(){
+        
+}
+}
